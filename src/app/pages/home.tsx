f5322c947f31af4052457616821f536219e5efde
GerrmanUNAP <ccus.german.chura@gmail.com>
2025-04-28 22:24:56 -0500

    chore: first views

---
+++ src/app/pages/home.tsx
@@ -0,0 +1,9 @@
+import { CardDemo } from "../components/card";
+export default function HomePage() {
+    return (
+        <div className="content-center">
+            This is the home page.
+            <CardDemo></CardDemo>
+        </div>
+    );
+}
