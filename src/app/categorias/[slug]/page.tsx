4ba956e267496ee3a531cbe3c837f9b385b0fe3e
GerrmanUNAP <ccus.german.chura@gmail.com>
2025-05-05 04:19:50 -0500

    fix: last change

--- src/app/categorias/[slug]/page.tsx
+++ src/app/categorias/[slug]/page.tsx
@@ -94,7 +94,7 @@ export default function CategoryStoresPage() {
 
   return (
     <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-bold mb-6">
+      <h1 className="text-xl font-bold mb-6">
         {categoryName
           ? `Tiendas en la categoría: ${categoryName}`
           : "Cargando categoría..."}
