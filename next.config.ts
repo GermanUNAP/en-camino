480ac1311a4f2314d0599f68024ee49b479cc34f
German Chura <ccus.german.chura@gmail.com>
2025-06-05 09:38:50 -0500

    chore: next config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        pathname: '/v0/b/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
-      },
-    ],
+    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
   },
   eslint: {
     ignoreDuringBuilds: true,
