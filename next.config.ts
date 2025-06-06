6dd280f74390c3ce67324743da75646b6258902c
German Chura <ccus.german.chura@gmail.com>
2025-06-05 22:26:47 -0500

    chore: Next config recover

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/v0/b/en-camino-777b7.firebasestorage.app/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
